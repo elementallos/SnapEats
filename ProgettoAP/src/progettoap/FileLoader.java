2893e775d0d7066b29379bc7b843b2de0f953194
gardean <gabrielardean@gmail.com>
2023-02-21 09:54:15 +0100

    Added Alert For Impiegati Error Check + New Logo

--- ProgettoAP/src/progettoap/FileLoader.java
+++ ProgettoAP/src/progettoap/FileLoader.java
@@ -4,18 +4,30 @@
  */
 package progettoap;
 
-/**
- *
- * @author HP
- */
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class FileLoader {
     
-    private String data;
-    
-    public FileLoader(){}
-    
-    public String getData(){
-        return data;
+    private String filename = null;
+    public FileLoader(String filename){
+        this.filename = filename;
     }
     
+    public void getFileData() {
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
 }
--- ProgettoAP/src/progettoap/filesFXML/Imp1Controller.java
+++ ProgettoAP/src/progettoap/filesFXML/Imp1Controller.java
@@ -4,8 +4,14 @@
  * and open the template in the editor.
  */
 package progettoap.filesFXML;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import progettoap.FileLoader;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -15,7 +21,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -28,6 +37,9 @@ public class Imp1Controller implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private String selectedFile;
+    private FileChooser chooser = new FileChooser();
+    private String filePath = "../fileData/";
     
     @FXML
     private TextField btn1;
@@ -57,8 +69,74 @@ public class Imp1Controller implements Initializable {
     }
     
     @FXML
-    public void caricaCV(ActionEvent event){
-        System.out.println("Loading <nome-cognome>_cv.txt --> ");
+    public void caricaCV() throws IOException{
+        
+        String nome, cognome;
+        nome = btn1.getText();
+        cognome = btn2.getText();
+        String filename = null;
+            
+        if(nome.equals("")!=true && cognome.equals("")!=true){
+            File f = chooser.showOpenDialog(null);
+            chooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+            selectedFile = f.getAbsolutePath();
+            System.out.println(f);
+            String newPath = "src/progettoap/fileData/";
+            File dir=new File(newPath);
+            if(!dir.exists()){
+                dir.mkdirs();
+            }
+            File sourceFile, destinationFile;
+            String extension = selectedFile.substring(selectedFile.lastIndexOf('.') + 1);
+
+
+            // fetch person info (name and surname)
+            nome = nome.toLowerCase();
+            cognome = cognome.toLowerCase();
+            filename = nome + "_" + cognome;
+            FileLoader CVData = new FileLoader(filename);
+        
+        
+            filePath += filename + "." + extension;
+            sourceFile = new File(selectedFile);
+            destinationFile = new File(newPath + filename + "." + extension);
+            //Files.copy(sourceFile.toPath(),destinationFile.toPath());
+            copyFileUsingStream(sourceFile,destinationFile);
+        }else{
+            showAlert();
+        }
+    }
+    
+    private void copyFileUsingStream(File source, File dest)  {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+            is.close();
+            os.close();
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
+    private void showAlert() {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Informazioni mancanti");
+
+        // Header Text: null
+        alert.setHeaderText(null);
+        alert.setContentText("Inserire almeno \'nome\' e \'cognome\'");
+
+        alert.showAndWait();
     }
     
     @FXML
